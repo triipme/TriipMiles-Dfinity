2fcc22a6334a061a74e312fe0ed9a77f35c62171
Mavis2103 <taquanvku@gmail.com>
2021-10-17 16:05:30 +0700

    fix

--- counter/.dfx/local/canisters/counter/counter.did.d.ts
+++
@@ -1,6 +0,0 @@
-import type { Principal } from '@dfinity/principal';
-export interface _SERVICE {
-  'get' : () => Promise<bigint>,
-  'inc' : () => Promise<bigint>,
-  'set' : (arg_0: bigint) => Promise<bigint>,
-}
