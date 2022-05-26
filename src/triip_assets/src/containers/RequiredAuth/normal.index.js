e94e03aced4f29d0a94deddc864e89a4ef276f12
Khanh Mac <khanh@triip.me>
2022-05-26 13:55:39 +0700

    Show unauthorized text

--- src/triip_assets/src/containers/RequiredAuth/normal.index.js
+++ src/triip_assets/src/containers/RequiredAuth/normal.index.js
@@ -1,11 +1,15 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 
 const NormalRequiredAuth = ({ children }) => {
   const { isLogin } = useSelector(state => state.user);
   const navigate = useNavigate();
   if (!isLogin) {
+    toast.error("You need to login first.", {
+      duration: 3000
+    });
     navigate("/");
   }
   return <>{children}</>;
